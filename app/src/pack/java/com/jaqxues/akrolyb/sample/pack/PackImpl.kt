b4485c3e5a319d0e465ed7064801465efb37c1a3
jaqxues <32979131+jaqxues@users.noreply.github.com>
2020-06-16 13:31:29 +0200

    Fix PackImpl.kt

--- app/src/pack/java/com/jaqxues/akrolyb/sample/pack/PackImpl.kt
+++ app/src/pack/java/com/jaqxues/akrolyb/sample/pack/PackImpl.kt
@@ -3,16 +3,17 @@ package com.jaqxues.akrolyb.sample.pack
 import android.content.Context
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.jaqxues.akrolyb.genhook.FeatureHelper
 import com.jaqxues.akrolyb.genhook.FeatureManager
-import com.jaqxues.akrolyb.sample.ipack.AMetadata
-import com.jaqxues.akrolyb.sample.ipack.AModPack
+import com.jaqxues.akrolyb.sample.ipack.ModPack
+import com.jaqxues.akrolyb.sample.ipack.PackMetadata
 
 
 /**
  * This file was created by Jacques Hoffmann (jaqxues) in the Project Akrolyb.<br>
  * Date: 20.04.20 - Time 12:25.
  */
-class PackImpl(metadata: AMetadata): AModPack(metadata) {
+class PackImpl(metadata: PackMetadata): ModPack(metadata) {
     override fun <T> customMethods(): T {
         throw NotImplementedError()
     }
@@ -23,9 +24,5 @@ class PackImpl(metadata: AMetadata): AModPack(metadata) {
 
     override fun showSuccessToast(context: Context) {
         Toast.makeText(context, "Pack loaded successfully", Toast.LENGTH_LONG).show()
-    }
-
-    override fun loadFeatureManager(): FeatureManager {
-        throw NotImplementedError()
     }
 }
