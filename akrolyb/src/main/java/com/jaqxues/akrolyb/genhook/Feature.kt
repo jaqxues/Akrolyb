af3923c3eeb74a552b63f1db1353a2db7035ca01
jaqxues <32979131+jaqxues@users.noreply.github.com>
2020-09-26 22:25:12 +0200

    Removing unused name StringResource

--- akrolyb/src/main/java/com/jaqxues/akrolyb/genhook/Feature.kt
+++ akrolyb/src/main/java/com/jaqxues/akrolyb/genhook/Feature.kt
@@ -10,10 +10,6 @@ import androidx.annotation.StringRes
  * Date: 16.03.20 - Time 15:34.
  */
 interface Feature {
-
-    val name: Int
-        @StringRes get
-
     fun loadFeature(classLoader: ClassLoader, context: Context)
 
     fun lateInit(classLoader: ClassLoader, activity: Activity)
